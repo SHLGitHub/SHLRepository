6a4c3e0c7a1a199804d946af04c9f08ac0b994c7
tom_glb <goodMorning_glb@atguigu.com>
2018-08-07 21:10:55 +0800

    hello word!!

--- monkey_rent_car/src/main/java/com/example/test/TestDemo.java
+++ monkey_rent_car/src/main/java/com/example/test/TestDemo.java
@@ -5,6 +5,7 @@ public class TestDemo {
     public static void main(String[] args) {
 
         System.out.println("test  demo");
+        System.out.println("hello word!!!!!!!!!!!!!!!!!!!!!push");
 
     }
 
