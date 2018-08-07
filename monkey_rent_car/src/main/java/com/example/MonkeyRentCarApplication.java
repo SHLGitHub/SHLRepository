65a86b336a745d30343f35f59ea5ba2f894f4aed
SHL <10-2-9-12@SHL.com>
2018-08-07 20:27:47 +0800

    test demo

---
+++ monkey_rent_car/src/main/java/com/example/MonkeyRentCarApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkeyRentCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonkeyRentCarApplication.class, args);
+	}
+}
